7597b536421227e7162496f638efb1dc4ddb4365
SHOURYA JHAVAR <shouryaj1206@gmail.com>
2026-09-18 11:53:07 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -19,10 +19,16 @@ module lut #(
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
   // Use an initial block with a for loop -- this is the only place a ROM's
   // contents should be set up. (See the lab manual for why.)
-  
+  initial begin
+        for (i = 0; i < DEPTH; i = i + 1)
+            mem[i] = i * i;
+    end
+
 
   // TODO: make dout continuously reflect mem[sel]. This is a combinational
   // read -- pick the right procedural block and sensitivity list.
 
-
+ always @(*) begin
+        dout = mem[sel];
+    end
 endmodule
--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,24 +4,33 @@
 module tb;
 
   // TODO: declare the inputs and outputs
-
+reg [1:0] t_sel;
+    wire [7:0] t_dout;
   // TODO: instantiate DUT here
-
+lut dut (
+        .sel(t_sel),
+        .dout(t_dout)
+    );
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, dut);
     end
   end
 
   initial begin
     // TODO: apply different input combinations
+t_sel = 2'b00; #5;
+        t_sel = 2'b01; #5;
+        t_sel = 2'b10; #5;
+        t_sel = 2'b11; #5;
 
+        $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%b | dout=%d", t_sel, t_dout);
 
 endmodule
