4f88e04618b3eb42b080e8ee4840448a6b2fd2c0
SHOURYA JHAVAR <shouryaj1206@gmail.com>
2026-09-18 11:43:12 +0000

    lab02 task1

--- labs/lab02/task1/dut.v
+++ labs/lab02/task1/dut.v
@@ -11,19 +11,19 @@ module DUT (
 );
 
   // ---- Option 1: dataflow version ----
-  mux_df U1 (
-    .I0 (I0),
-    .I1 (I1),
-    .S  (S),
-    .Y  (Y)
-  );
-
-  // ---- Option 2: behavioral version ----
-  // mux_beh U1 (
+  // mux_df U1 (
   //   .I0 (I0),
   //   .I1 (I1),
   //   .S  (S),
   //   .Y  (Y)
   // );
+
+  // ---- Option 2: behavioral version ----
+  mux_beh U1 (
+    .I0 (I0),
+    .I1 (I1),
+    .S  (S),
+    .Y  (Y)
+  );
 
 endmodule
